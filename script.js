c031a53974d8ec96e088ddfe0942050aa43a4cc2
fouad ajayi <fouadajayi6@gmail.com>
2022-10-31 23:26:38 +0100

    Inplementing Funtionalities

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ btnRoll.addEventListener('click' , function(){
 
 
 btnHold.addEventListener( 'click' , function(){
+
     if (playing){
 
         // 1. Add current score to Active player
@@ -88,7 +89,7 @@ btnHold.addEventListener( 'click' , function(){
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 100) {
             playing = false
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
